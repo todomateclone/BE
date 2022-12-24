c664971807860682f18a48a1adcf2cf286c6f09c
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2022-12-24 18:06:07 +0900

    Feature/cicd (#10)
    * feat : CI/CD 구축
    
    * feat : CI/CD 구축
    
    CI/CD 구축했습니다
    
    * feat : github secret 추가
    
    * fix : gradle.yml Github Secrets 관련 내용 수정
    
    * feat : cicd init

--- src/main/java/com/team6/todomateclone/common/HealthyCheckController.java
+++ src/main/java/com/team6/todomateclone/common/HealthyCheckController.java
@@ -3,13 +3,15 @@ package com.team6.todomateclone.common;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/")
 public class HealthyCheckController {
 
     @GetMapping
-    public ResponseEntity healthyCheck() {
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity<HttpStatus> healthyCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
