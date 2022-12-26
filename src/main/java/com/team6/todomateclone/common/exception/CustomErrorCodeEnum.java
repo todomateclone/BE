aa396fb1781537431941bff22a124333dfb7f61b
hangj97 <gksruwls@naver.com>
2022-12-26 11:15:01 +0900

    refactor : 커스텀 에러 코드 이넘 JWT 예외 변수명 변경

--- src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
+++ src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
@@ -12,8 +12,8 @@ public enum CustomErrorCodeEnum {
     TAG_NOT_FOUND("Tag를 찾을 수 없습니다."),
 
     /* JWT 예외 메시지 */
-    TOKEN_NOT_FOUND( "토큰이 존재하지 않습니다."),
-    INVALID_TOKEN("토큰이 유효하지 않습니다.");
+    TOKEN_NOT_FOUND_MSG( "토큰이 존재하지 않습니다."),
+    INVALID_TOKEN_MSG("토큰이 유효하지 않습니다.");
 
     private final String msg;
 
