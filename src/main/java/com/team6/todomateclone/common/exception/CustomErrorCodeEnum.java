de4d39b8f041fef7904f8152db1a7a6325adbc99
jinni <105099062+hangj97@users.noreply.github.com>
2022-12-26 11:17:13 +0900

    feat : Security 설정 (#15)
    * add : JWT 시크릿 키 작업 환경 분리한 파일 추가
    
    * comment : 주석 해제
    
    * feat : 시큐리티 CORS, 패스워드 인코더 추가
    
    * add : JWT 예외 메시지 추가
    
    * feat : 토큰 검증하는 필터 추가
    
    * feat : JWT 생성 클래스 추가
    
    * feat : JWT 커스텀 에러 핸들러 추가
    
    * feat : 인증 완료된 유저 객체를 사용하는 클래스 추가
    
    * feat : 인증 객체를 통해 유저 객체를 검증 및 인증 완료된 유저 객체 반환 클래스 추가
    
    * add : 이메일을 기준으로 유저를 검색하는 메서드 추가
    
    * refactor : 커스텀 에러 코드 이넘 JWT 예외 변수명 변경

--- src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
+++ src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
@@ -10,7 +10,11 @@ public enum CustomErrorCodeEnum {
     TODO_INVALID_PERMISSION("Todo 권한이 유효하지 않습니다."),
 
     /* Tag 예외 메시지 */
-    TAG_NOT_FOUND("Tag를 찾을 수 없습니다.");
+    TAG_NOT_FOUND("Tag를 찾을 수 없습니다."),
+
+    /* JWT 예외 메시지 */
+    TOKEN_NOT_FOUND_MSG( "토큰이 존재하지 않습니다."),
+    INVALID_TOKEN_MSG("토큰이 유효하지 않습니다.");
 
     private final String msg;
 
