2db2640aaf7f769f8d415613d2db1bf5dc181c1f
hangj97 <gksruwls@naver.com>
2022-12-26 10:37:32 +0900

    add : JWT 예외 메시지 추가

--- src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
+++ src/main/java/com/team6/todomateclone/common/exception/CustomErrorCodeEnum.java
@@ -1,9 +1,6 @@
 package com.team6.todomateclone.common.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Getter
 public enum CustomErrorCodeEnum {
@@ -12,7 +9,11 @@ public enum CustomErrorCodeEnum {
     TODO_INVALID_PERMISSION("Todo 권한이 유효하지 않습니다."),
 
     /* Tag 예외 메시지 */
-    TAG_NOT_FOUND("Tag를 찾을 수 없습니다.");
+    TAG_NOT_FOUND("Tag를 찾을 수 없습니다."),
+
+    /* JWT 예외 메시지 */
+    TOKEN_NOT_FOUND( "토큰이 존재하지 않습니다."),
+    INVALID_TOKEN("토큰이 유효하지 않습니다.");
 
     private final String msg;
 
