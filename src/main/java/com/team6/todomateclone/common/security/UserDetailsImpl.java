b19570f0facc278353330fc2dd2713afd50ccec9
hangj97 <gksruwls@naver.com>
2022-12-26 10:41:18 +0900

    feat : 인증 완료된 유저 객체를 사용하는 클래스 추가

--- src/main/java/com/team6/todomateclone/common/security/UserDetailsImpl.java
+++ src/main/java/com/team6/todomateclone/common/security/UserDetailsImpl.java
@@ -1,4 +1,59 @@
 package com.team6.todomateclone.common.security;
 
-public class UserDetailsImpl {
+import com.team6.todomateclone.member.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final Member member; // 인증 완료된 User 객체
+    private final String email; // 인증 완료된 User email
+    private final String password; // 인증 완료된 User password
+
+    public UserDetailsImpl(Member member, String email, String password) {
+        this.member = member;
+        this.email = email;
+        this.password = password;
+    }
+
+    // 인증 완료된 User 를 가져오는 Getter
+    public Member getMember() {
+        return member;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
