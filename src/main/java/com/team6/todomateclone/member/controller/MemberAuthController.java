8392fd78c939ae5e58b41ecf8ac3d1947d277f56
suhjaesuk <suhjaesuk@gmail.com>
2022-12-26 10:41:33 +0900

    fix : 회원가입 로직 변경 - 기존 회원 가입 시 회원 정보를 request 받아서 저장 -변경 회원 가입 시 회원 정보 default 값을 저장

--- src/main/java/com/team6/todomateclone/member/controller/MemberAuthController.java
+++ src/main/java/com/team6/todomateclone/member/controller/MemberAuthController.java
@@ -4,11 +4,10 @@ import com.team6.todomateclone.common.response.SuccessResponse;
 import com.team6.todomateclone.member.dto.RequestSignupMemberDto;
 import com.team6.todomateclone.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -16,10 +15,11 @@ import java.io.IOException;
 public class MemberAuthController {
 
     private final MemberService memberService;
-    @PostMapping(value = "/signup", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public SuccessResponse<Object> signup(@RequestPart RequestSignupMemberDto request, @RequestPart MultipartFile multipartFile) throws IOException {
-        memberService.signup(request, multipartFile);
+    @PostMapping("/signup")
+    public SuccessResponse<Object> signup(@RequestBody RequestSignupMemberDto request){
+        memberService.signup(request);
         return new SuccessResponse<>("회원가입 성공하였습니다.",null);
+
     }
 
 }
