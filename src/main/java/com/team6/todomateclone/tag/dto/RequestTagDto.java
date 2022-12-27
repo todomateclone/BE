34e7d6e69d8901ea156eb805a822c6dbbade9570
SeDevel <ohhojin25@naver.com>
2022-12-27 12:11:18 +0900

    feat : Swagger 설정(Tag)

--- src/main/java/com/team6/todomateclone/tag/dto/RequestTagDto.java
+++ src/main/java/com/team6/todomateclone/tag/dto/RequestTagDto.java
@@ -1,13 +1,17 @@
 package com.team6.todomateclone.tag.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "태그 Requset Dto")
 @Getter
 @NoArgsConstructor
 public class RequestTagDto {
-
+    @ApiModelProperty(value = "태그 이름", dataType = "String", example = "다이어트")
     private String tagName;
+    @ApiModelProperty(value = "태그 색상", dataType = "String", example = "#000000")
     private String tagColor;
 
 }
