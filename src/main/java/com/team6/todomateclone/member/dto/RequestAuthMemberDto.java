02a53d50aa0a9a8ffb8496d2363a0882517441cf
hangj97 <gksruwls@naver.com>
2022-12-27 12:42:27 +0900

    refactor : 로그인, 회원가입의 Dto를 RequestAuthDto로 수정

--- src/main/java/com/team6/todomateclone/member/dto/RequestAuthMemberDto.java
+++ src/main/java/com/team6/todomateclone/member/dto/RequestAuthMemberDto.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Getter
-public class RequestSignupMemberDto {
+public class RequestAuthMemberDto {
     @NotBlank
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$", message = "이메일 형식이 올바르지 않습니다.")
     private String email;
