32440223cc300070ba1c2b713c1b286f43ef2bee
jinni <105099062+hangj97@users.noreply.github.com>
2022-12-27 14:24:22 +0900

    ♻️ Refactor : 로그인, 회원가입의 Dto를 RequestAuthDto로 수정 (#36)
    * refactor : 로그인, 회원가입의 Dto를 RequestAuthDto로 수정
    
    * fix : 사용하지 않는 주석으로 인해 에러 발생
    
    * remove : RequestLoginDto 삭제

--- src/main/java/com/team6/todomateclone/member/dto/RequestAuthMemberDto.java
+++ src/main/java/com/team6/todomateclone/member/dto/RequestAuthMemberDto.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Pattern;
 
 @ApiModel(value = "회원가입 Request Dto")
 @Getter
-public class RequestSignupMemberDto {
+public class RequestAuthMemberDto {
     @ApiModelProperty(value = "회원 이메일", dataType = "String", example = "test@naver.com")
     @NotBlank
     @Pattern(regexp = "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$", message = "이메일 형식이 올바르지 않습니다.")
