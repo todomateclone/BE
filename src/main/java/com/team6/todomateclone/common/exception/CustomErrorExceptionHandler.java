f9dc29dbc032a991feeb841474df457e7e82a129
SeDevel <ohhojin25@naver.com>
2022-12-28 22:54:01 +0900

    refactor : 허가되지 않은 HTTP 요청 및 글로벌 예외 구현

--- src/main/java/com/team6/todomateclone/common/exception/CustomErrorExceptionHandler.java
+++ src/main/java/com/team6/todomateclone/common/exception/CustomErrorExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.team6.todomateclone.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,13 +13,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
+@Slf4j
 public class CustomErrorExceptionHandler {
 
+    /** Custom 예외 핸들러 **/
     @ExceptionHandler(value = {CustomErrorException.class})
     protected ResponseEntity<CustomErrorResponse> handleCustomException(CustomErrorException e) {
         return CustomErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
+    /** Validation 예외처리 **/
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     protected ResponseEntity<CustomErrorResponse> handleValidException(MethodArgumentNotValidException e) {
         return CustomErrorResponse.toResponseValidEntity(e.getBindingResult().getFieldError().getDefaultMessage());
@@ -26,4 +34,16 @@ public class CustomErrorExceptionHandler {
         return CustomErrorResponse.toResponseValidEntity(e.getConstraintViolations().iterator().next().getMessage());
     }
 
+    /** 지원하지 않는 HTTP 요청 예외처리 **/
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    protected ResponseEntity<CustomErrorResponse> handleCustomHttpRequestMethodNotSupportException() {
+        return CustomErrorResponse.toResponseValidEntity("비정상적인 요청입니다.");
+    }
+
+    /** 그외 나머지 예외 처리 **/
+    @ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<CustomErrorResponse> handleCustomGlobalException(Exception e) {
+        log.info(e.getMessage()); //예외 로그
+        return CustomErrorResponse.toResponseValidEntity("비정상적인 요청입니다.");
+    }
 }
